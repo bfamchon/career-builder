27e54ed137af0f30ce1d0d89b943a44daf426e16
bfamchon <baptiste.famchon@thetribe.io>
2022-08-30 12:58:52 +0200

    :fix: sonar issues

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const helmet = require("helmet");
+
 const app = express();
 
+app.use(helmet());
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function (req, res) {
